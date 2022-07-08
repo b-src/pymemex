8bec2278da31a03697badaa3864a9e7bcd736e5e
Brett Gleason <brettmgleason@gmail.com>
2022-07-07 19:19:06 -0500

    add markdown tokenizer tests for the rest of the syntax that has been defined. many are failing as is. whether that is due to bad test setup or tokenizer bugs is TBD

--- test/test_markdown_tokenizer.py
+++ test/test_markdown_tokenizer.py
@@ -79,3 +79,193 @@ def test_markdown_tokenizer_handles_h6(markdown_tokenizer, tmp_path):
     
     with open(temp_file_path, mode="r") as f:
         assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_single_linebreak(markdown_tokenizer, tmp_path):
+    test_markdown = "\nthis\nis\na\nstring\nwith\nsingle\nlinebreaks"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["\n", "this", "\n", "is", "\n", "a", "\n", "string", "\n", "with", "\n", "single", "\n", "linebreaks"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_paragraph_separator(markdown_tokenizer, tmp_path):
+    test_markdown = "\n\n\n\nthis is a paragraph\n\nsecond paragraph\n\n\n\n"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["\n\n", "this is a paragraph", "\n\n", "second paragraph", "\n\n", "\n\n"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_linebreak(markdown_tokenizer, tmp_path):
+    test_markdown = "explicit\\\\\\linebreak"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["explicit", "\\\\\\", "linebreak"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_bold(markdown_tokenizer, tmp_path):
+    test_markdown = "this is *bold* text"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["this is ", "*", "bold", "*", " text"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_italic(markdown_tokenizer, tmp_path):
+    test_markdown = "this is **italic** text"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["this is ", "**", "italic", "**", " text"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_bold_italic(markdown_tokenizer, tmp_path):
+    test_markdown = "this is ***bold-italic*** text"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["this is ", "**", "bold-italic", "**", " text"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_internal_link(markdown_tokenizer, tmp_path):
+    test_markdown = "link here {internal link (who knows what this format looks like?)}"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["link here ", "{", "internal link (who knows what this format looks like?)", "}"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_external_link(markdown_tokenizer, tmp_path):
+    test_markdown = "link here [external link (who knows what this format looks like?)]"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["link here ", "[", "external link (who knows what this format looks like?)", "]"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_quote(markdown_tokenizer, tmp_path):
+    test_markdown = 'quote incoming ""blah blah blah (attribution)""'
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["quote incoming ", '""', "blah blah blah (attribution)", '""']
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_citation(markdown_tokenizer, tmp_path):
+    test_markdown = "{{source}}"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["{{", "source", "}}"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_figure(markdown_tokenizer, tmp_path):
+    test_markdown = "[[path to figure (description)]]"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["[[", "path to figure (description)", "]]"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_inline_code(markdown_tokenizer, tmp_path):
+    test_markdown = "here is some `inline code`"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["here is some ", "`", "inline code", "`"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_multiline_code(markdown_tokenizer, tmp_path):
+    test_markdown = "here is some ```multi\nline\ncode```"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["here is some ", "```", "multi", "\n", "line", "\n", "code", "```"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_backslash(markdown_tokenizer, tmp_path):
+    test_markdown = "explicit\\\\backslash"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["explicit", "\\\\", "backslash"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_octothorpe(markdown_tokenizer, tmp_path):
+    test_markdown = "explicit\\#octothorpe"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["explicit", "\\#", "octothorpe"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_octothorpe(markdown_tokenizer, tmp_path):
+    test_markdown = "explicit\\#octothorpe"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["explicit", "\\#", "octothorpe"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_asterisk(markdown_tokenizer, tmp_path):
+    test_markdown = "explicit\\*asterisk"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["explicit", "\\*", "asterisk"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_curly_braces(markdown_tokenizer, tmp_path):
+    test_markdown = "\\{text in curly braces\\}"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["\\{", "text in curly braces", "\\}"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
+
+
+def test_markdown_tokenizer_handles_explicit_square_brackets(markdown_tokenizer, tmp_path):
+    test_markdown = "\\[text in square brackets\\]"
+    temp_file_path = write_data_to_temp_file(test_markdown, tmp_path)
+        
+    expected_result = ["\\[", "text in square brackets", "\\]"]
+    
+    with open(temp_file_path, mode="r") as f:
+        assert markdown_tokenizer.tokenize_markdown(f) == expected_result
